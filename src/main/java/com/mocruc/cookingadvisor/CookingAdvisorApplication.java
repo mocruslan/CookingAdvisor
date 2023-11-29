d3712bbdd2991c535d935c5f29093b7d527d7921
ruslan <mochulskyy.ruslan@gmail.com>
2023-11-29 19:39:06 +0100

    Init

---
+++ src/main/java/com/mocruc/cookingadvisor/CookingAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.mocruc.cookingadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mocruc/cookingadvisor/CookingAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocruc.cookingadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
